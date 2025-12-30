5565506c01b785088176f8d54aa402b14748ae44
isabellaaddas <isabellaaddas@gmail.com>
2025-12-29 20:07:42 -0500

    initial class setup

--- Caesar Cipher/CaesarCipher.cpp
+++ Caesar Cipher/CaesarCipher.cpp
@@ -2,3 +2,26 @@
 
 #include "CaesarCipher.h"
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+CaesarCipher::CaesarCipher() {
+
+}
+
+CaesarCipher::CaesarCipher(int newShift) {
+
+}
+
+void CaesarCipher::encrypt(string message) {
+
+}
+
+const string CaesarCipher::decrypt(string message) {
+
+}
+
+CaesarCipher::~CaesarCipher() {
+
+}
--- Caesar Cipher/CaesarCipher.h
+++ Caesar Cipher/CaesarCipher.h
@@ -3,8 +3,21 @@
 #ifndef CPP_PROJECTS_CAESARCIPHER_H
 #define CPP_PROJECTS_CAESARCIPHER_H
 
+#include <string>
+using namespace std;
 
 class CaesarCipher {
+public:
+    CaesarCipher();
+    CaesarCipher(int newShift);
+    ~CaesarCipher();
+    void encrypt(string message);
+    const string decrypt(string message);
+private:
+    int shift;
+    char alphaArray[26];
+    char shiftArray[26];
+    string direction;
 };
 
 
