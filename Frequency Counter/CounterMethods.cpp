421f18b03b34afc00fbf537293de5b484083a74f
isabellaaddas <isabellaaddas@gmail.com>
2026-01-02 22:19:37 -0500

    added countChars() definition in CounterMethods.cpp

--- Frequency Counter/CounterMethods.cpp
+++ Frequency Counter/CounterMethods.cpp
@@ -1,1 +1,32 @@
 // Isabella Addas, 1/2/26
+
+#include "CounterMethods.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*** Function definitions ***/
+
+// Counts characters from input string and returns the total
+// (not including spaces)
+const int countChars(string input) {
+
+    // Initialize count variable to 0
+    int count(0);
+
+    // Iterate over the entire length of the input string
+    for (int i = 0; i < input.length(); i++) {
+
+        // If a space is encountered, exclude from character
+        // count
+        if (input[i] == ' ') {
+            continue;
+        }
+
+        // Otherwise, add character to count
+        count++;
+    }
+
+    return count;
+}
