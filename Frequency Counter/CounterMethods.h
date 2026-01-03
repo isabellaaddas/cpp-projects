52dd348ab332f097a14eb22207b15143a7bc010e
isabellaaddas <isabellaaddas@gmail.com>
2026-01-02 22:13:33 -0500

    added function prototypes in CounterMethods.h

--- Frequency Counter/CounterMethods.h
+++ Frequency Counter/CounterMethods.h
@@ -3,4 +3,16 @@
 #ifndef CPP_PROJECTS_COUNTERMETHODS_H
 #define CPP_PROJECTS_COUNTERMETHODS_H
 
+#include <string>
+
+using namespace std;
+
+// Function prototypes for counting methods
+const int countChars(string input);
+const int countWords(string input);
+const int countCharsWithSpace(string input);
+const int countLines(string input);
+const int countChar(string input, char target);
+const int countWord(string input, string target);
+
 #endif //CPP_PROJECTS_COUNTERMETHODS_H
