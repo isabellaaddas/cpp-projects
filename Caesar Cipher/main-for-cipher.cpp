30e06a87a8d68e03a1e2d99695620a648e5d5292
isabellaaddas <isabellaaddas@gmail.com>
2026-01-31 22:41:51 -0500

    added test for encode(string) in main method

--- Caesar Cipher/main-for-cipher.cpp
+++ Caesar Cipher/main-for-cipher.cpp
@@ -8,9 +8,12 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
     CaesarCipher test = CaesarCipher();
+    string message = "hello";
 
     // Set up test values
     char tChar1 = test.translate('A');
+    string tMsg1 = test.encode(message);
 
     cout << "What is 'A' when there is a left shift of 3?: " << tChar1 << endl;
+    cout << "What is 'hello' when there is a left shift of 3?: " << tMsg1 << endl;
 }
