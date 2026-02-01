448e8808fe2a827b081e1fd94c7baa6dfeb57264
isabellaaddas <isabellaaddas@gmail.com>
2026-01-31 22:17:08 -0500

    added calculation logic to left shift case in translate(char)

--- Caesar Cipher/CaesarCipher.cpp
+++ Caesar Cipher/CaesarCipher.cpp
@@ -39,11 +39,28 @@ const string CaesarCipher::decode(string message) {
 // a different char value (all alphabetical) based on
 // the shift
 const char CaesarCipher::translate(char let) {
+    char transChar('\0');
 
     // Two cases of calculations: left or right shift
     // Split up with if-else branch
     if (direction == "left") {
 
+        // In left shift, all char values will have the
+        // shift number subtracted from them
+
+        // Split up based on char value range (lowercase
+        // and uppercase values accepted!)
+        if ((let >= 65) && (let <= 90)) {
+
+            // When the char value - shift is under 65,
+            // must add the length of the alphabet
+            // to wrap around the letters
+            transChar = ((let - shift) < 65) ? (let - shift) + 26 : let - shift;
+        } else if ((let >= 97) && (let <= 122)) {
+
+            // The same logic applies to lowercase range
+            transChar = ((let - shift) < 97) ? (let - shift) + 26 : let - shift;
+        }
     } else {
 
     }
