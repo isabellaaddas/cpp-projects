8e65f4cece928e9637c4184a4676d4829fbeb26b
isabellaaddas <isabellaaddas@gmail.com>
2026-01-31 22:31:54 -0500

    added test for translate(char) method in main method

--- Caesar Cipher/main-for-cipher.cpp
+++ Caesar Cipher/main-for-cipher.cpp
@@ -8,4 +8,9 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
     CaesarCipher test = CaesarCipher();
+
+    // Set up test values
+    char tChar1 = test.translate('A');
+
+    cout << "What is 'A' when there is a left shift of 3?: " << tChar1 << endl;
 }
