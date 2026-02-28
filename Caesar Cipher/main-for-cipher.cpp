caa1dbb0d52face10291233016fc88c3b75b3a7c
isabellaaddas <isabellaaddas@gmail.com>
2026-02-28 00:57:46 -0500

    added test case for decode method

--- Caesar Cipher/main-for-cipher.cpp
+++ Caesar Cipher/main-for-cipher.cpp
@@ -9,11 +9,14 @@ using namespace std;
 int main (int argc, char *argv[]) {
     CaesarCipher test = CaesarCipher();
     string message = "hello";
+    string secretMessage = "ebiil";
 
     // Set up test values
     char tChar1 = test.translate('A');
     string tMsg1 = test.encode(message);
+    string tMsg2 = test.decode(secretMessage);
 
     cout << "What is 'A' when there is a left shift of 3?: " << tChar1 << endl;
     cout << "What is 'hello' when there is a left shift of 3?: " << tMsg1 << endl;
+    cout << "What is 'ebiil' when there is a left shift of 3?: " << tMsg2 << endl;
 }
